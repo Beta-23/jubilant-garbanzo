0f4bc73211fc73a6b97f2f0632da829f86ea9170
Beta-23 <alfred.dominguez@gmail.com>
2016-01-31 17:44:37 -0800

    Adds jQuery to app.js

--- app.js
+++ app.js
@@ -3,20 +3,21 @@
 var distance;
 var refNode;
 
-
-
 window.onload = function() {
     console.log("linked");
-    document.getElementById("contribute").addEventListener("submit", catchelement);
-
+    // document.getElementById("contribute").addEventListener("submit", catchelement); ///<--this code line is without jQuery//
+    //Below using jQuery//
+    $("#contribute").on("submit", catchelement); 
 };
 
 function createElement () {
-        var $li = document.createElement("li");
+     var $li = $("#li");
     $li.textContent = distance;
-    var ul = document.getElementById("Post-ul");
-    ul.appendChild($li); 
-    document.getElementById("Post-list").insertBefore($li, refNode);
+    // No jQuery-->var ul = document.getElementById("Post-ul");
+
+    $("#Post-ul").prepend("<li>" + distance + "</li>"); 
+    // No jQuery-->ul.appendChild($li); 
+    // No jQuery-->document.getElementById("Post-list").insertBefore($li, refNode);
     refNode = $li; 
 
 }
@@ -25,9 +26,11 @@ function catchelement(event){
      console.log(this);
      //var count;
 	event.preventDefault();
-    // distance = $('#id').value; //trying to jquery getElementByID//not working//
-     distance = document.getElementById("comment").value;
+    // No jQuery--> distance = document.getElementById("comment").value;//
+     //using jQuery//
+     distance = $("#comment").val();
      createElement();
+     console.log(this);
      this.reset();
      //localStorage.setItem('input' + count, input); count++;//console error, trying to define count//
      
